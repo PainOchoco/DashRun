299aa43766d8815bf98423ae945a13dc25b0034d
PainOchoco <matteo.planchet@gmail.com>
2021-04-30 09:20:25 +0200

    🧩 Added simple map & jump

--- dashrun.py
+++ dashrun.py
@@ -3,22 +3,93 @@ import sys
 from pygame.locals import *
 
 pygame.init()
-display = pygame.display
 clock = pygame.time.Clock()
 
-display.set_caption("Dash Run")
+pygame.display.set_caption("Dash Run")
 
-size = (400, 400)
+WINDOW_SIZE = (400, 400)
 
 # ? Initialise la fenêtre
-screen = pygame.display.set_mode(size, 0, 32)
+screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
+
+display = pygame.Surface((300, 300))
 
 # ? Initialise le joueur
 player_image = pygame.image.load("./assets/player/no_animation.png")
-player_location = [50, 50]
+
+grass_image = pygame.image.load('./assets/grass.png')
+TILE_SIZE = grass_image.get_width()
+
+dirt_image = pygame.image.load('./assets/dirt.png')
+
+game_map = [['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '2', '2', '2',
+                '2', '2', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '0', '0'],
+            ['2', '2', '0', '0', '0', '0', '0', '0', '0', '0',
+                '0', '0', '0', '0', '0', '0', '0', '2', '2'],
+            ['1', '1', '2', '2', '2', '2', '2', '2', '2', '2',
+                '2', '2', '2', '2', '2', '2', '2', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1',
+                '1', '1', '1', '1', '1', '1', '1', '1', '1'],
+            ['1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1', '1']]
+
+
+def get_hit_tile(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+
+def move(rect, movement, tiles):
+    collision_types = {"top": False, "bottom": False,
+                       "right": False, "left": False}
+    rect.x += movement[0]
+
+    hit_list = get_hit_tile(rect, tiles)
+
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types["right"] = True
+
+        if movement[0] < 0:
+            rect.left = tile.right
+            collision_types["left"] = True
+
+    rect.y += int(movement[1])
+    hit_list = get_hit_tile(rect, tiles)
+
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types["bottom"] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types["top"] = True
+
+    return rect, collision_types
+
+
 player_y_momentum = 0
+air_timer = 0
 player_entity = pygame.Rect(
-    player_location[0], player_location[1], player_image.get_width(), player_image.get_height())
+    50, 50, player_image.get_width(), player_image.get_height())
 
 moving_right = False
 moving_left = False
@@ -28,34 +99,49 @@ test_rectangle = pygame.Rect(100, 100, 100, 50)
 # ? Boucle du jeu
 while True:
     # ? Efface l'écran
-    screen.fill((4, 44, 54))
+    display.fill((4, 44, 54))
+
+    tile_rects = []
+
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == "1":
+                display.blit(dirt_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile == "2":
+                display.blit(grass_image, (x * TILE_SIZE, y * TILE_SIZE))
+            if tile != "0":
+                tile_rects.append(pygame.Rect(
+                    x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
 
     # ? Affiche le joueur
-    screen.blit(
-        player_image, (int(player_location[0]), int(player_location[1])))
 
-    # ? Mouvements
+    player_movement = [0, 0]
     if moving_right:
-        player_location[0] += 4
-
+        player_movement[0] += 2
     if moving_left:
-        player_location[0] -= 4
+        player_movement[0] -= 2
 
-    # ? Collision bordure
-    if player_location[1] > size[1] - player_image.get_height():
-        player_y_momentum = -player_y_momentum
+    player_movement[1] += player_y_momentum
+    player_y_momentum += 0.2
+
+    if player_y_momentum > 3:
+        player_y_momentum = 3
+
+    player_entity, collisions = move(
+        player_entity, player_movement, tile_rects)
+
+    if collisions["bottom"]:
+        player_y_momentum = 0
+        air_timer = 0
     else:
-        player_y_momentum += 0.2
+        air_timer += 1
 
-    player_location[1] += player_y_momentum
-
-    player_entity.x = int(player_location[0])
-    player_entity.y = int(player_location[1])
-
-    if player_entity.colliderect(test_rectangle):
-        pygame.draw.rect(screen, (255, 0, 0), test_rectangle)
-    else:
-        pygame.draw.rect(screen, (0, 0, 0), test_rectangle)
+    display.blit(
+        player_image, (int(player_entity.x), int(player_entity.y)))
 
     for event in pygame.event.get():
         # ? Ferme le programme
@@ -71,6 +157,10 @@ while True:
             # ? <-
             if event.key == K_LEFT:
                 moving_left = True
+            # ? ^
+            if event.key == K_UP:
+                if air_timer < 6:  # ? Le joueur peut sauter s'il est dans les airs moins de 6 frames
+                    player_y_momentum = -5
 
         # ? Touche lâchée
         if event.type == KEYUP:
@@ -81,5 +171,6 @@ while True:
             if event.key == K_LEFT:
                 moving_left = False
 
-    display.update()
+    screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0, 0))
+    pygame.display.update()
     clock.tick(60)  # ? 60FPS
